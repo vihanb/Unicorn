e4ece980ac18acf640e7cb386b46088a63c657d0
Vihan Bhargava <blue1086@gmail.com>
2015-12-28 12:14:42 -0800

    Update unicorn.js

--- unicorn.js
+++ unicorn.js
@@ -1,6 +1,7 @@
 function Unicorn(code){
   var chars = code.split(" ").map(function(ch) {
-    return ch!=="🦄"?ch:String.fromCharCode(ch.length);
+	var emoji = ch.match(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g)[0];
+	  return ch.charCodeAt()<5000?ch:emoji==="🦄"?String.fromCharCode(ch.length/2):emoji==="🐐"?String.fromCharCode(ch.length*2):ch
   }).join("");
   return eval(chars);
 }
