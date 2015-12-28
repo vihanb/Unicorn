b715dff3ff8b7b8a3b4dc099591bac114ea46669
Vihan Bhargava <blue1086@gmail.com>
2015-12-28 12:34:00 -0800

    Update unicorn.js

--- unicorn.js
+++ unicorn.js
@@ -1,4 +1,5 @@
 function Unicorn(code){
+	if (code.indexOf("!") || code.indexOf('"')) code = RemoveEncoding(code);
   var chars = code.split(" ").map(function(ch) {
 	var emoji = ch.match(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g)[0];
 	  return ch.charCodeAt()<5000?ch:emoji==="🦄"?String.fromCharCode(ch.length/2):emoji==="🐐"?String.fromCharCode(ch.length*2):ch
@@ -10,4 +11,16 @@ function GenerateUnicorn(JavaScriptCode) {
 return JavaScriptCode.split``.map(function(l){
 	return l.charCodeAt()%4?"🦄".repeat(l.charCodeAt()):"🐐".repeat(l.charCodeAt()/4)
 }).join` `;
+}
+
+function ApplyEncoding(code) {
+	return code.match(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g).map(function(l){
+		return l==="🦄"?'\\':l===" "?" ":"🐐";
+	}).join("")
+}
+
+function RemoveEncoding(code) {
+	return code.match(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g).map(function(l){
+		return l==='\\'?"🦄":l===" "?" ":"!";
+	}).join("")
 }
