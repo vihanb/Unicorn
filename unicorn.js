748160a065ba78a81cbf63d4c6e886e979e6d5eb
Vihan Bhargava <blue1086@gmail.com>
2015-12-28 22:03:48 -0800

    Update unicorn.js

--- unicorn.js
+++ unicorn.js
@@ -1,13 +1,13 @@
 function Unicorn(code) {
   var chars = code.split(" ").map(function(ch) {
     var emoji = ch.match(/🌈|✨|🦄|🐐/g)[0];
-    return ch.charCodeAt() < 5000 ? "" : emoji === "🌈" ? String.fromCharCode(ch.length * 2) : emoji === "✨" ? String.fromCharCode((ch.length/2)*3) : emoji === "🦄" ? String.fromCharCode(ch.length / 2) : emoji === "🐐" ? String.fromCharCode(ch.length) : "" 
+    return ch.charCodeAt() < 5000 ? "" : emoji === "🌈" ? String.fromCharCode(ch.length * 2) : emoji === "✨" ? String.fromCharCode(ch.length * 3) : emoji === "🦄" ? String.fromCharCode(ch.length / 2) : emoji === "🐐" ? String.fromCharCode(ch.length) : "" 
   }).join("");
-  return eval(chars);
+  return chars;
 }
 
 function GenerateUnicorn(JavaScriptCode) {
   return JavaScriptCode.split ``.map(function(l) {
-    return l.charCodeAt() % 4 ? "🌈".repeat(l.charCodeAt()) : l.charCodeAt() % 3 ? "✨".repeat(l.charCodeAt()) : l.charCodeAt() % 2 ? "🦄".repeat(l.charCodeAt()) : "🐐".repeat(l.charCodeAt() / 2)
+    return l.charCodeAt() % 4 === 0 ? "🌈".repeat(l.charCodeAt()/4) : l.charCodeAt() % 3 === 0 ? "✨".repeat(l.charCodeAt() / 3) : l.charCodeAt() % 2 ? "🦄".repeat(l.charCodeAt()) : "🐐".repeat(l.charCodeAt() / 2)
   }).join ` `;
 }
